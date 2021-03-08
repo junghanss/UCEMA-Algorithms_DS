95f342e279df4f025a74080375fc31ec59649159
Juan Cruz Junghanss <64294082+junghanss@users.noreply.github.com>
2021-03-08 20:51:44 -0300

    Recursion Basic Exercises

---
+++ Algorithms/TP Recursividad - Libre/TP Recursividad - Libre/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace TP_Recursividad___Libre
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.Ventana1 = new System.Windows.Forms.ListBox();
+            this.Ventana2 = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.Ventana3 = new System.Windows.Forms.ListBox();
+            this.Ventana4 = new System.Windows.Forms.ListBox();
+            this.Ventana5 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AccessibleDescription = "";
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(47, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Tasa de Interés";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(282, 36);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 42);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Ackermann";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(47, 204);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(130, 42);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Reversa String";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(282, 203);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(130, 42);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Minúsculas";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Ventana1
+            // 
+            this.Ventana1.FormattingEnabled = true;
+            this.Ventana1.Location = new System.Drawing.Point(28, 99);
+            this.Ventana1.Name = "Ventana1";
+            this.Ventana1.Size = new System.Drawing.Size(170, 69);
+            this.Ventana1.TabIndex = 8;
+            this.Ventana1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Ventana2
+            // 
+            this.Ventana2.FormattingEnabled = true;
+            this.Ventana2.Location = new System.Drawing.Point(282, 99);
+            this.Ventana2.Name = "Ventana2";
+            this.Ventana2.Size = new System.Drawing.Size(130, 69);
+            this.Ventana2.TabIndex = 9;
+            this.Ventana2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(542, 204);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(130, 42);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "Comparación";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Ventana3
+            // 
+            this.Ventana3.FormattingEnabled = true;
+            this.Ventana3.Location = new System.Drawing.Point(28, 269);
+            this.Ventana3.Name = "Ventana3";
+            this.Ventana3.Size = new System.Drawing.Size(170, 69);
+            this.Ventana3.TabIndex = 11;
+            this.Ventana3.SelectedIndexChanged += new System.EventHandler(this.Ventana3_SelectedIndexChanged);
+            // 
+            // Ventana4
+            // 
+            this.Ventana4.FormattingEnabled = true;
+            this.Ventana4.Location = new System.Drawing.Point(260, 269);
+            this.Ventana4.Name = "Ventana4";
+            this.Ventana4.Size = new System.Drawing.Size(176, 69);
+            this.Ventana4.TabIndex = 12;
+            this.Ventana4.SelectedIndexChanged += new System.EventHandler(this.Ventana4_SelectedIndexChanged);
+            // 
+            // Ventana5
+            // 
+            this.Ventana5.FormattingEnabled = true;
+            this.Ventana5.Location = new System.Drawing.Point(508, 269);
+            this.Ventana5.Name = "Ventana5";
+            this.Ventana5.Size = new System.Drawing.Size(202, 69);
+            this.Ventana5.TabIndex = 13;
+            this.Ventana5.SelectedIndexChanged += new System.EventHandler(this.listBox5_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(937, 450);
+            this.Controls.Add(this.Ventana5);
+            this.Controls.Add(this.Ventana4);
+            this.Controls.Add(this.Ventana3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.Ventana2);
+            this.Controls.Add(this.Ventana1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox Ventana1;
+        private System.Windows.Forms.ListBox Ventana2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListBox Ventana3;
+        private System.Windows.Forms.ListBox Ventana4;
+        private System.Windows.Forms.ListBox Ventana5;
+    }
+}
+
---
+++ Algorithms/TP Recursividad - Libre/TP Recursividad - Libre/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace TP_Recursividad___Libre
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Ventana1.Items.Clear();
+                double _inputTasa = double.Parse(Interaction.InputBox("Ingrese la tasa de interés correspondiente: ")) / 100;
+                int _inputPeriodos = int.Parse(Interaction.InputBox("Ingrese la cantidad de períodos: "));
+                double _output = (TasaDeRetorno(_inputTasa, _inputPeriodos) - 1)*100;
+                _output = Math.Round(_output,3);
+                MessageBox.Show("La tasa de interés compuesta es " + _output + "%", "Resultado");
+                Ventana1.Items.Add("Tasa simple: " + _inputTasa);
+                Ventana1.Items.Add("Cantidad períodos: " + _inputPeriodos);
+                Ventana1.Items.Add("Tasa compuesta: " + _output + "%");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }   // Tasa de interés compuesta
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Ventana2.Items.Clear();
+                MessageBox.Show("Recuerde que la función de Ackermann es sensible a números enteros mayores a 4, tenga a bien ingresar números menores.", "Precaución");
+                int _input1 = int.Parse(Interaction.InputBox("Ingrese el primer número"));
+                int _input2 = int.Parse(Interaction.InputBox("Ingrese el segundo número"));
+                if (_input1 > 4 || _input2 >= 4)
+                {
+                    MessageBox.Show("Ingresaste números mayores que 5 en alguna entrada, pruebe con enteros menores por favor.");
+                    _input1 = int.Parse(Interaction.InputBox("Ingrese el primer número"));
+                    _input2 = int.Parse(Interaction.InputBox("Ingrese el segundo número"));
+                }
+                int _output = Ackermann(_input1, _input2);
+                MessageBox.Show("Para los numeros " + _input1 + " y " + _input2 + ", el resultado de la función Ackermann es: " + _output, "Resultado");
+                Ventana2.Items.Add("Primer número: " + _input1);
+                Ventana2.Items.Add("Segundo número: " + _input2);
+                Ventana2.Items.Add("Resultado Ackermann: " + _output);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }   // Función de Ackermann
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Ventana3.Items.Clear();
+                string _input = Interaction.InputBox("Ingrese la palabra por transformar: ");
+                string _output = ReversaPrueba(_input, 0);
+                MessageBox.Show("El resultado de la palabra " + _input + " es: " + _output);
+                Ventana3.Items.Add("Palabra ingresada: " + _input);                
+                Ventana3.Items.Add("Resultado: " + _output);               
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }   // Transformación Reversa String
+        private void Ventana3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Ventana4.Items.Clear();
+                string _input = Interaction.InputBox("Ingrese la palabra en mayúsculas a modificar: ");
+                bool _flag = MinusculasBool(_input);
+                if (_flag == true) { MessageBox.Show("La palabra ya está completamente en minúsculas.", "Resultado"); }
+                else
+                {
+                    string _output = Minusculas(_input);
+                    MessageBox.Show("El resultado es: " + _output, "Resultado");
+                    Ventana4.Items.Add("Palabra ingresada: " + _input);
+                    Ventana4.Items.Add("Resultado: " + _output);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }   // Transformación a Minusculas
+        private void Ventana4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Ventana5.Items.Clear();
+                string _input1 = Interaction.InputBox("Ingrese la primer palabra a evaluar: ");
+                string _input2 = Interaction.InputBox("Ingrese la segunda palabra a evaluar: ");
+                bool _output = ComparacionStrings(_input1, _input2, 0); // el parámetro Posición debe cargarse como cero la primera vez. No dejamos que el usuario lo haga.
+                MessageBox.Show("La validación entre la palabra '" + _input1 + "' y '" + _input2 + "' es: " + _output, "Resultado");
+                Ventana5.Items.Add("Primer palabra: " + _input1);
+                Ventana5.Items.Add("Segunda palabra: " + _input2);
+                Ventana5.Items.Add("Resultado: " + _output);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }   // Comparacion Dos Strings 
+        private void listBox5_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }   
+
+
+
+        public double TasaDeRetorno(double tasa, int periodos) 
+        {
+            // Recursiva para calcular una tasa de interés compuesta (tiempo discreto)
+            if (periodos == 0) { return 1.0; } // Caso Base
+            else // Caso Recursivo
+            { return ((1.0 + tasa) * TasaDeRetorno(tasa, periodos - 1)); }
+        } 
+        public int Ackermann(int n, int m) 
+        {
+            // Función de Ackermann... ojo con enteros mayores a 4 por OverFlow...
+            if (n == 0) 
+            { return m + 1; }
+
+            if (n != 0 && m == 0)
+            { return Ackermann(n - 1, 1); } 
+
+            else 
+            { return Ackermann(n - 1, Ackermann(n, m - 1)); }
+
+        } // Funcion de Ackermann
+        public bool MinusculasBool(string input)
+        {
+            // Función para determinar si un string ya está escrito completamente en minúsculas
+            int _largo = input.Length;
+            int _contador = 0;
+            bool _flag;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Char.IsLower(input[i]) == true) { _contador++; }             
+            }
+            if(_contador == _largo) { _flag = true; }
+            else { _flag = false; }
+
+            return _flag;
+        }       // Función auxiliar de validacion para recursiva Minusculas
+        public string Minusculas(string palabra)
+        {
+            // Función recursiva para modificar un string a minúsculas.
+            if (palabra.Length == 1) { return palabra.ToLower(); } // Caso Base: 1 solo caracter
+
+            else
+            {   // Caso General/Recursivo: substrae de a una letra, la convierte en minuscula y la resta del string para hacer el llamado recursivo
+                string letra;
+                letra = palabra.Substring(0, 1);
+                letra= letra.ToLower();
+                palabra = palabra.Remove(0, 1);
+                return letra + Minusculas(palabra);
+            }
+        }
+        public bool ComparacionStrings(string pPalabraPrimera, string pPalabraSegunda, int pPosicion)
+        {
+            // Funcion recursiva para validar igualdad en dos strings. Se cumple si:
+            // (1) Las palabras poseen el mismo largo + (2) Las palabras tengan las mismas letras
+            if (pPalabraPrimera.Length != pPalabraSegunda.Length) { return false; } // Validamos condición necesaria
+
+            else
+            {
+                int _largo = pPalabraPrimera.Length;
+                if (pPosicion < _largo) // Validación para efectuar llamado recursivo o cortar
+                {
+                    if(pPalabraPrimera[pPosicion] != pPalabraSegunda[pPosicion])    // Validación de 2da condición necesaria - Primer Potencial Corte de la función
+                    {
+                        return false;                         
+                    }
+                    else
+                    {
+                        // Llamado recursivo
+                        pPosicion = pPosicion + 1;
+                        return ComparacionStrings(pPalabraPrimera, pPalabraSegunda, pPosicion);
+                    }  
+                }
+
+                else // Segundo Potencial Corte de la función - La posición recorrió los strings completos: el resultado es true
+                { return true; }                
+            }
+
+        }
+        public string ReversaPrueba(string pPalabra, int pPosicion)
+        {
+            // Método recursivo para reversa de una cadena de strings (orden reversivo)
+            if(pPosicion < pPalabra.Length)
+            {
+                return pPalabra[pPalabra.Length - pPosicion - 1].ToString() + ReversaPrueba(pPalabra, pPosicion + 1);
+            }
+            else
+            {
+                return "";
+            }
+        }
+        public string ReversaString(string pPalabra)
+        {
+            // Método alternativo sin auxiliar contador
+            if (pPalabra.Length > 0) // Caso Recursivo 
+            { return pPalabra[pPalabra.Length - 1] + ReversaString(pPalabra.Substring(0, pPalabra.Length - 1)); }
+
+            else { return pPalabra; } // Condición de Corte
+        }   // Metodo alternativo a ReversaPrueba
+    }
+    
+}
+
+
+
