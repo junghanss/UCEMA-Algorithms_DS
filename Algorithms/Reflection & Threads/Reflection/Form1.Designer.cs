4d4a124312431bbd94f4140faf5ab32dd6f3b82e
Juan Cruz Junghanss <64294082+junghanss@users.noreply.github.com>
2021-03-08 20:38:53 -0300

    Reflection Program

---
+++ Algorithms/Reflection & Threads/Reflection/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Reflection
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(167, 98);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(262, 148);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(26, 149);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Caracteristicas ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(26, 28);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 42);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "GetType";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(26, 346);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 42);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Propiedades";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 18;
+            this.listBox2.Location = new System.Drawing.Point(167, 262);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(262, 238);
+            this.listBox2.TabIndex = 3;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(504, 346);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(120, 42);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Metodos";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listBox3
+            // 
+            this.listBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 18;
+            this.listBox3.Location = new System.Drawing.Point(645, 262);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(262, 238);
+            this.listBox3.TabIndex = 5;
+            this.listBox3.SelectedIndexChanged += new System.EventHandler(this.listBox3_SelectedIndexChanged);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(504, 28);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(120, 42);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Interfaces";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // listBox4
+            // 
+            this.listBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.ItemHeight = 18;
+            this.listBox4.Location = new System.Drawing.Point(645, 8);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(262, 94);
+            this.listBox4.TabIndex = 7;
+            this.listBox4.SelectedIndexChanged += new System.EventHandler(this.listBox4_SelectedIndexChanged);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(504, 152);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(120, 42);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Campos";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // listBox5
+            // 
+            this.listBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.ItemHeight = 18;
+            this.listBox5.Location = new System.Drawing.Point(645, 132);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(262, 94);
+            this.listBox5.TabIndex = 9;
+            this.listBox5.SelectedIndexChanged += new System.EventHandler(this.listBox5_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(969, 544);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.listBox5);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ListBox listBox5;
+    }
+}
+
---
+++ Algorithms/Reflection & Threads/Reflection/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+using Microsoft.VisualBasic;
+
+namespace Reflection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Type t;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e) { }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Ingresamos el nombre específico para obtener el objeto Type
+            t = Type.GetType(Interaction.InputBox("Ingrese la clase u objeto: "));
+            MessageBox.Show("Nombre ingresado: " + t, "Resultado");
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Llamamos a la función características
+            listBox1.Items.Clear();
+            CaracteristicasTipo(t);
+        }
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e) { }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Llamamos a la funcion para encontrar las propiedades
+            listBox2.Items.Clear();
+            EncuentraPropiedades(t);
+        }
+        private void listBox3_SelectedIndexChanged(object sender, EventArgs e) { }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Llamamos a la funcion para encontrar los metodos
+            listBox3.Items.Clear();
+            EncuentraMetodos(t);
+        }
+        private void listBox4_SelectedIndexChanged(object sender, EventArgs e) { }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            // Llamamos a la funcion para encontrar interfaces 
+            listBox4.Items.Clear();
+            EncuentraInterfaces(t);
+        }
+        private void listBox5_SelectedIndexChanged(object sender, EventArgs e) { }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            // Llamamos a la funcion para campos
+            listBox5.Items.Clear();
+            EncuentraCampos(t);
+        }
+
+        #region "Funciones"
+        public void CaracteristicasTipo(Type t)
+        {
+            listBox1.Items.Add("Las caracteristicas son: ");
+            listBox1.Items.Add("Clase base: " + t.BaseType);
+            listBox1.Items.Add("Es clase: " + t.IsClass);
+            listBox1.Items.Add("Abstracta: " + t.IsAbstract);
+            listBox1.Items.Add("Sellada: " + t.IsSealed);
+            listBox1.Items.Add("Generica: " + t.IsGenericType);
+        }
+
+        public void EncuentraPropiedades(Type t)
+        {
+            listBox2.Items.Add("Las propiedades son: ");
+            var nombres = from p in t.GetProperties() select p.Name;
+            foreach(var nombre in nombres)
+            {
+                listBox2.Items.Add(nombre);
+            }
+        }
+
+        public void EncuentraMetodos(Type t)
+        {
+            listBox3.Items.Add("Los metodos son: ");
+            var metodos = from m in t.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static) select m.Name;
+            foreach(var metodo in metodos)
+            {
+                listBox3.Items.Add(metodo);
+            }
+        }
+
+        public void EncuentraInterfaces(Type t)
+        {
+            listBox4.Items.Add("Las interfaces son: ");
+            var interfaces = from i in t.GetInterfaces() select i;
+            foreach(var interfaz in interfaces)
+            {
+                listBox4.Items.Add(interfaz);
+            }
+        }
+
+        public void EncuentraCampos(Type t)
+        {
+            listBox5.Items.Add("Los campos son: ");
+            var campos = from f in t.GetFields() select f.Name;
+            foreach(var campo in campos)
+            {
+                listBox5.Items.Add(campo);
+            }
+        }
+
+        #endregion
+
+
+    }
+
+
+
+}
