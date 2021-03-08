434d3c1d7a70b39a67ba8ada8d7a0712216036c7
Juan Cruz Junghanss <64294082+junghanss@users.noreply.github.com>
2021-03-08 20:39:50 -0300

    Threading Program

---
+++ Algorithms/Reflection & Threads/Subproc_Threads/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Subproc_Threads
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(165, 115);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(88, 65);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(288, 115);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(88, 65);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(408, 115);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(88, 65);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(213, 229);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 36);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(344, 229);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 36);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Algorithms/Reflection & Threads/Subproc_Threads/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Subproc_Threads
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        System.Threading.Thread _AlgoritmoUno;
+        System.Threading.Thread _AlgoritmoDos;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //System.Threading.ThreadStart _TStartAlgoritmoUno = new System.Threading.ThreadStart(AlgoritmoUno);
+            Form.CheckForIllegalCrossThreadCalls = false;
+            _AlgoritmoUno = new System.Threading.Thread(new System.Threading.ThreadStart(AlgoritmoUno));
+            _AlgoritmoDos = new System.Threading.Thread(new System.Threading.ThreadStart(AlgoritmoDos));
+            
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (_AlgoritmoUno.IsAlive) { startUno = startDos = true; _AlgoritmoUno.Resume(); _AlgoritmoDos.Resume(); }
+            else
+            {
+                startUno = startDos = true; _AlgoritmoUno.Start(); _AlgoritmoDos.Start();
+            }
+            button1.Enabled = false;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            startUno = startDos = false; _AlgoritmoUno.Suspend(); _AlgoritmoDos.Suspend();
+            button1.Enabled = true;
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        int resultadoAlgoritmoUno = 2, resultadoAlgoritmoDos = 1, resultadoAlgoritmoTres = 2;
+
+
+        bool startUno = false, startDos = false, startTres = false;
+
+        #region "Safe Call on Form Controls"
+
+        private delegate void SafeCallDelegate(string text);
+        private void WriteTextSafe(string text)
+        {
+            if (textBox1.InvokeRequired)
+            {
+                var d = new SafeCallDelegate(WriteTextSafe);
+                textBox1.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                while (startUno)
+                {
+                    //textBox1.Text = text;
+                    textBox1.Text = resultadoAlgoritmoUno.ToString(); textBox1.Show();
+                    resultadoAlgoritmoUno = resultadoAlgoritmoUno + 2;
+                }
+            }
+        }
+        private void SetText()
+        {
+            WriteTextSafe("This text was set safely.");
+        }
+
+        #endregion
+
+        private void AlgoritmoUno()
+        {
+            while (startUno)
+            {
+                textBox1.Text = resultadoAlgoritmoUno.ToString(); textBox1.Show();
+                resultadoAlgoritmoUno = resultadoAlgoritmoUno + 2;
+                System.Threading.Thread.Sleep(2000);
+            }
+        }
+
+        private void AlgoritmoDos()
+        {
+            while (startDos)
+            {
+                textBox2.Text = resultadoAlgoritmoDos.ToString(); textBox2.Show();
+                resultadoAlgoritmoDos = resultadoAlgoritmoDos + 2;
+                System.Threading.Thread.Sleep(2000);
+            }
+        }
+    }
+}
