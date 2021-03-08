b40838ee3b0878a2e050ddadf558d8567e0663e1
Juan Cruz Junghanss <64294082+junghanss@users.noreply.github.com>
2021-03-08 20:46:43 -0300

    Sockets Program

---
+++ Algorithms/Socket/EjemploServidor/ServidorForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace EjemploServidor
+{
+    partial class ServidorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEnviarMensaje = new System.Windows.Forms.Button();
+            this.txtMensaje = new System.Windows.Forms.TextBox();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.btnVerConectados = new System.Windows.Forms.Button();
+            this.txtConectados = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnEnviarMensaje
+            // 
+            this.btnEnviarMensaje.Location = new System.Drawing.Point(409, 12);
+            this.btnEnviarMensaje.Name = "btnEnviarMensaje";
+            this.btnEnviarMensaje.Size = new System.Drawing.Size(127, 49);
+            this.btnEnviarMensaje.TabIndex = 3;
+            this.btnEnviarMensaje.Text = "Enviar mensaje a todos los clientes";
+            this.btnEnviarMensaje.UseVisualStyleBackColor = true;
+            this.btnEnviarMensaje.Click += new System.EventHandler(this.btnEnviarMensaje_Click);
+            // 
+            // txtMensaje
+            // 
+            this.txtMensaje.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMensaje.Location = new System.Drawing.Point(12, 12);
+            this.txtMensaje.Name = "txtMensaje";
+            this.txtMensaje.Size = new System.Drawing.Size(391, 21);
+            this.txtMensaje.TabIndex = 2;
+            this.txtMensaje.TextChanged += new System.EventHandler(this.txtMensaje_TextChanged);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLog.Location = new System.Drawing.Point(12, 67);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtLog.Size = new System.Drawing.Size(524, 431);
+            this.txtLog.TabIndex = 4;
+            this.txtLog.WordWrap = false;
+            this.txtLog.TextChanged += new System.EventHandler(this.txtLog_TextChanged);
+            // 
+            // btnVerConectados
+            // 
+            this.btnVerConectados.Location = new System.Drawing.Point(542, 12);
+            this.btnVerConectados.Name = "btnVerConectados";
+            this.btnVerConectados.Size = new System.Drawing.Size(96, 49);
+            this.btnVerConectados.TabIndex = 5;
+            this.btnVerConectados.Text = "Ver clientes conectados";
+            this.btnVerConectados.UseVisualStyleBackColor = true;
+            this.btnVerConectados.Click += new System.EventHandler(this.btnVerConectados_Click);
+            // 
+            // txtConectados
+            // 
+            this.txtConectados.Location = new System.Drawing.Point(542, 67);
+            this.txtConectados.Multiline = true;
+            this.txtConectados.Name = "txtConectados";
+            this.txtConectados.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtConectados.Size = new System.Drawing.Size(245, 158);
+            this.txtConectados.TabIndex = 6;
+            this.txtConectados.WordWrap = false;
+            this.txtConectados.TextChanged += new System.EventHandler(this.txtConectados_TextChanged);
+            // 
+            // ServidorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 510);
+            this.Controls.Add(this.txtConectados);
+            this.Controls.Add(this.btnVerConectados);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnEnviarMensaje);
+            this.Controls.Add(this.txtMensaje);
+            this.Name = "ServidorForm";
+            this.Text = "Servidor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnviarMensaje;
+        private System.Windows.Forms.TextBox txtMensaje;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Button btnVerConectados;
+        private System.Windows.Forms.TextBox txtConectados;
+    }
+}
+
---
+++ Algorithms/Socket/EjemploServidor/ServidorForm.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemploServidor
+{
+    public partial class ServidorForm : Form
+    {
+        Servidor servidor;
+
+        public ServidorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Log(string texto)
+        {
+            // Invoke nos permite ejecutar un delegado en el tread de la UI. 
+            // El problema radica en que no es seguro interactuar con los controles
+            // de Windows Forms desde múltiples threads. Y en este ejemplo, el 
+            // método Log se está llamando desde eventos que se disparan desde
+            // threads creados en el objeto Servidor.
+            // Ver: https://docs.microsoft.com/en-us/dotnet/framework/winforms/controls/how-to-make-thread-safe-calls-to-windows-forms-controls
+            Invoke((Action)delegate
+            {
+                txtLog.AppendText($"{DateTime.Now.ToLongTimeString()} - {texto}");
+                txtLog.AppendText(Environment.NewLine); // Lo reemplazamos por el \n\r porque estaba funcionando mal...
+            });
+        }
+        private void Log2(string texto)
+        {
+            Invoke((Action)delegate
+            {
+                txtConectados.AppendText($"{DateTime.Now.ToLongTimeString()} - {texto}");
+                txtConectados.AppendText(Environment.NewLine);
+            });
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Inicializo el servidor estableciendo el puerto donde escuchar
+            servidor = new Servidor(8050);
+
+            // Me suscribo a los eventos
+            servidor.NuevaConexion += Servidor_NuevaConexion;
+            servidor.ConexionTerminada += Servidor_ConexionTerminada;
+            servidor.DatosRecibidos += Servidor_DatosRecibidos;
+            servidor.ClientesConectados += Servidor_ClientesConectados;
+
+            // Comienzo la escucha
+            servidor.Escuchar();
+        }
+
+        private void Servidor_NuevaConexion(object sender, ServidorEventArgs e)
+        {
+            //  Muestro quién se conectó
+            Log($"Se ha conectado un nuevo cliente desde la IP = {e.EndPoint.Address}, Puerto = {e.EndPoint.Port}");
+        }
+
+        private void Servidor_ConexionTerminada(object sender, ServidorEventArgs e)
+        {
+            // Muestro con quién se terminó la conexión
+            Log($"Se ha desconectado el cliente de la IP = {e.EndPoint.Address}, Puerto = {e.EndPoint.Port}");
+        }
+
+        private void Servidor_DatosRecibidos(object sender, DatosRecibidosEventArgs e)
+        {
+            if(e.DatosRecibidos.)
+            // Muestro quién envió el mensaje
+            Log($"Nuevo mensaje desde el cliente de la IP = {e.EndPoint.Address}, Puerto = {e.EndPoint.Port}");
+
+            //  Muestro el mensaje recibido
+            Log(e.DatosRecibidos);
+
+            // Juancito: por si queremos que el server repita los mensajes...
+            //Log("Mensaje reenviado a todos los clientes."); servidor.EnviarDatos(e.DatosRecibidos);
+        }
+
+        private void Servidor_ClientesConectados(object sender, ClientesConectadosEventArgs e)
+        {
+            txtConectados.Clear();
+            Log2("Los clientes conectados al momento son: ");
+            int i = 0;
+            foreach(var dato in e.ArrayEndPoint) 
+            {
+                Log2($"Cliente IP = {e.ArrayEndPoint[i].Address}, Puerto = {e.ArrayEndPoint[i].Port}");                
+                i++;
+            }
+            
+            
+        }
+        private void btnEnviarMensaje_Click(object sender, EventArgs e)
+        {
+            servidor.EnviarDatos(txtMensaje.Text);
+            txtMensaje.Clear();
+        }
+
+        private void txtLog_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void txtMensaje_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        // Juancito: controles de windows form para mostrar clientes conectados
+        private void btnVerConectados_Click(object sender, EventArgs e)
+        {
+            servidor.MostrarClientesConectados();
+        }
+        private void txtConectados_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
