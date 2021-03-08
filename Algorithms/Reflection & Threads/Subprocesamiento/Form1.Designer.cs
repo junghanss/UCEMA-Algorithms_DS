32e8318203ad1f1d1025f1aa6fa9add245a3efe3
Juan Cruz Junghanss <64294082+junghanss@users.noreply.github.com>
2021-03-08 20:42:13 -0300

    Reflection and Threads Program

---
+++ Algorithms/Reflection & Threads/Subprocesamiento/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace Subprocesamiento
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker3 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker4 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(39, 192);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(136, 52);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(218, 192);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(136, 52);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(396, 192);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(136, 52);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(573, 192);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(136, 52);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(52, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 32);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "SumaPares";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(234, 24);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 32);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "SumaImpar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(413, 24);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 32);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Primos";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(589, 24);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(105, 32);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Mitad";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // StartButton
+            // 
+            this.StartButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartButton.Location = new System.Drawing.Point(249, 355);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(105, 33);
+            this.StartButton.TabIndex = 8;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StopButton.Location = new System.Drawing.Point(396, 355);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(105, 33);
+            this.StopButton.TabIndex = 9;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            // 
+            // backgroundWorker3
+            // 
+            this.backgroundWorker3.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker3_DoWork);
+            // 
+            // backgroundWorker4
+            // 
+            this.backgroundWorker4.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker4_DoWork);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(39, 259);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(136, 12);
+            this.progressBar1.TabIndex = 10;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ Algorithms/Reflection & Threads/Subprocesamiento/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Subprocesamiento
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool startBG1 = false, startBG2 = false, startBG3 = false, startBG4 = false;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form.CheckForIllegalCrossThreadCalls = false;
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+            backgroundWorker1.ProgressChanged +=
+                new ProgressChangedEventHandler(
+            backgroundWorker1_ProgressChanged);
+
+            backgroundWorker2.WorkerReportsProgress = true;
+            backgroundWorker2.WorkerSupportsCancellation = true;
+            backgroundWorker3.WorkerReportsProgress = true;
+            backgroundWorker3.WorkerSupportsCancellation = true;
+        }
+        int resultadoAlgoritmoUno = 2;
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int _progreso = 0;
+            while (startBG1)
+            {
+                System.Threading.Thread.Sleep(2000);
+                textBox1.Text = resultadoAlgoritmoUno.ToString();
+                textBox1.Show();
+                resultadoAlgoritmoUno = resultadoAlgoritmoUno + 2;
+                backgroundWorker1.ReportProgress(_progreso = _progreso + 1);
+            }
+        }
+        int resultadoAlgoritmoDos = 1;
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (startBG2)
+            {
+                System.Threading.Thread.Sleep(2000);
+                textBox2.Text = resultadoAlgoritmoDos.ToString();
+                textBox2.Show();
+                resultadoAlgoritmoDos = resultadoAlgoritmoDos + 2;
+            }
+        }
+        int resultadoAlgoritmoTres = 2;
+        private void backgroundWorker3_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (startBG3)
+            {
+                
+                if (resultadoAlgoritmoTres%2==0 || resultadoAlgoritmoTres%3==0 || resultadoAlgoritmoTres%5==0) // Es divisible por 2, 3 o 5? Ya no es primo...
+                {
+                    if(resultadoAlgoritmoTres==2 || resultadoAlgoritmoTres==3 || resultadoAlgoritmoTres==5) // Si es el mismisimo 2, 3 o 5, entonces son una excepcion
+                    { 
+                        textBox3.Text = resultadoAlgoritmoTres.ToString(); textBox3.Show(); resultadoAlgoritmoTres++;
+                        System.Threading.Thread.Sleep(2000); // Solo seteamos el sleep cuando imprime en pantalla
+                    }
+                    else { resultadoAlgoritmoTres++; } // Para el caso 'n' solamente le sumamos uno...                    
+                }
+                else
+                {
+                    // Es primo (dado que no es divisible por dos, tres o cinco...)
+                    textBox3.Text = resultadoAlgoritmoTres.ToString(); textBox3.Show(); resultadoAlgoritmoTres++;
+                    System.Threading.Thread.Sleep(2000);
+                }
+                //System.Threading.Thread.Sleep(2000);
+            }
+        }
+        private void backgroundWorker4_DoWork(object sender, DoWorkEventArgs e)
+        {
+            System.Threading.Thread.Sleep(20000);
+        }
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.IsBusy != true && backgroundWorker2.IsBusy != true && backgroundWorker3.IsBusy != true)
+            {
+                backgroundWorker1.RunWorkerAsync();
+                backgroundWorker2.RunWorkerAsync();
+                backgroundWorker3.RunWorkerAsync();
+                //StartButton.Enabled = false; StopButton.Enabled = true;
+                
+            }
+            startBG1 = startBG2 = startBG3 = startBG4 = true;
+        }
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.WorkerSupportsCancellation == true)
+            { 
+                backgroundWorker1.CancelAsync();
+                backgroundWorker2.CancelAsync();
+                backgroundWorker3.CancelAsync();
+                //StopButton.Enabled = false; StartButton.Enabled = true;
+            }
+            startBG1 = startBG2 = startBG3 = startBG4 = false;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (backgroundWorker1.IsBusy != true)
+                {
+                    backgroundWorker1.RunWorkerAsync(); startBG1 = true;
+                    MessageBox.Show("Comienzo del proceso asincronico.", "Suma Números Pares");
+                }
+                else
+                {
+                    backgroundWorker1.CancelAsync(); startBG1 = false;
+                    MessageBox.Show("Finalización del proceso asincronico.", "Suma Números Pares");
+                }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e) { }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (backgroundWorker2.IsBusy != true)
+                {
+                    backgroundWorker2.RunWorkerAsync(); startBG2 = true;
+                    MessageBox.Show("Comienzo del proceso asincronico.", "Suma Números Impares");
+                }
+                else
+                {
+                    backgroundWorker2.CancelAsync(); startBG2 = false;
+                    MessageBox.Show("Finalización del proceso asincronico.", "Suma Números Impares");
+                }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e) { }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (backgroundWorker3.IsBusy != true)
+                {
+                    backgroundWorker3.RunWorkerAsync(); startBG3 = true;
+                    MessageBox.Show("Comienzo del proceso asincronico.", "Mostrar Números Primos");
+                }
+                else
+                {
+                    backgroundWorker3.CancelAsync(); startBG3 = false;
+                    MessageBox.Show("Finalización del proceso asincronico.", "Mostrar Números Primos");
+                }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e) { }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (backgroundWorker4.IsBusy != true)
+                {
+                    backgroundWorker4.RunWorkerAsync(); startBG4 = true;
+                    MessageBox.Show("Comienzo del proceso asincronico.", "Mostrar Mitad del Número");
+                }
+                else
+                {
+                    backgroundWorker4.CancelAsync(); startBG4 = false;
+                    MessageBox.Show("Finalización del proceso asincronico.", "Mostrar Mitad del Número");
+                }
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+        private void textBox4_TextChanged(object sender, EventArgs e) { }
+
+
+
+
+        // This event handler updates the progress bar.
+        private void backgroundWorker1_ProgressChanged(object sender,
+            ProgressChangedEventArgs e)
+        {
+            this.progressBar1.Value = e.ProgressPercentage;
+        }
+    }
+
+
+
+
+}
